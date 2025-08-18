92ad087814d274049b99909c57a995636714b88b
samuel uzan <uzan98@gmail.com>
2025-08-17 21:09:47 -0300

    bugfix de data

--- src/components/estudos/StudySessionModal.tsx
+++ src/components/estudos/StudySessionModal.tsx
@@ -319,7 +319,6 @@ const StudySessionModal: React.FC<StudySessionModalProps> = ({
                     onChange={(e) => setScheduledDate(e.target.value)}
                     className="w-full pl-10 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     required
-                    min={new Date().toISOString().split('T')[0]}
                   />
                 </div>
               </div>
@@ -416,4 +415,4 @@ const StudySessionModal: React.FC<StudySessionModalProps> = ({
   );
 };
 
-export default StudySessionModal; 
+export default StudySessionModal;
